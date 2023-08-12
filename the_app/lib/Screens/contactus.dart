04cc858efff47145dd4a5ac1071c6fa82d9548b1
sola <solatijani8@gmail.com>
2023-08-12 07:06:12 +0100

    contact has been added

--- the_app/lib/Screens/contactus.dart
+++ the_app/lib/Screens/contactus.dart
@@ -4,6 +4,33 @@ class ContactUs extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SafeArea(
+      child: MaterialApp(
+        home:Scaffold(
+          appBar: AppBar(
+            title: Text('Contact Us',style: TextStyle(color: Colors.deepOrangeAccent),),
+          centerTitle: true,
+          backgroundColor: Colors.black,
+          ),
+          body:Container(
+            child: Column(
+              children: [
+                Card(
+                  color:  Colors.deepOrangeAccent,
+                  child: Padding(
+                    padding: EdgeInsets.all(20.0),
+                    child: Row(
+                      children: [
+                        Icon(Icons.phone),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        )
+      )
+    );
   }
 }
